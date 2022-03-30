cfd3eb3692bb04c4842c598858b8eaa77f1541f8
GaahNoel <ga.noel@outlook.com>
2022-03-30 08:41:11 -0300

    chore: fix docker-compose

--- src/main/factories/controller/account/signup-controller.factory.ts
+++ src/main/factories/controller/account/signup-controller.factory.ts
@@ -1,7 +1,6 @@
 import { DbAddAccount } from '@/data/usecases/account/db-add-account';
 import { BcryptAdapter } from '@/infra/cryptography';
 import { AccountPrismaRepository } from '@/infra/db/prisma/account/account-prisma.repository';
-import { env } from '@/main/config/env';
 import { SignUpController } from '@/presentation/controller/account/signup.controller';
 import { BaseController } from '@/presentation/protocols';
 
