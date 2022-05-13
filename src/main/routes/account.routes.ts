1572e452def1ee10eb04f0bf9a37438244467bbd
GaahNoel <ga.noel@outlook.com>
2022-05-13 09:35:32 -0300

    feat: add auth to edit account route and add docs with swagger

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -4,11 +4,12 @@ import { makeSignUpController, makeVerifyAccountController } from '@/main/factor
 import { makeCheckAccountPasswordController } from '@/main/factories/controller/account/check-account-password-controller.factory';
 import { makeEditAccountController } from '../factories/controller/account/edit-account-controller.factory';
 import { makeGetAccountByEmailController } from '../factories/controller/account/get-account-by-email-controller.factory';
+import { auth } from '@/main/middlewares/auth';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()));
   router.put('/account/verify', adaptRoute(makeVerifyAccountController()));
   router.post('/account/check-password', adaptRoute(makeCheckAccountPasswordController()));
-  router.put('/account/edit/:id', adaptRoute(makeEditAccountController()));
+  router.put('/account/edit/:id', auth(), adaptRoute(makeEditAccountController()));
   router.get('/account/email', adaptRoute(makeGetAccountByEmailController()));
 };
