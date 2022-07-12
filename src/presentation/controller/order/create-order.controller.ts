d1e93a66b82d0517fd15b97024a99fdb5b260736
GaahNoel <ga.noel@outlook.com>
2022-07-12 15:55:30 -0300

    feat: add new verifications to create order route

--- src/presentation/controller/order/create-order.controller.ts
+++ src/presentation/controller/order/create-order.controller.ts
@@ -1,8 +1,12 @@
+import { EmailVerificationSender } from '@/data/protocols/email/email-verification-sender';
 import { OrderItem } from '@/domain/models/order';
+import { GetAccountById } from '@/domain/usecases/account';
 import { ListBusinessById } from '@/domain/usecases/business';
 import { CreateOrder } from '@/domain/usecases/order';
-import { MissingParamsError } from '@/presentation/errors';
-import { badRequest, internalServerError, success } from '@/presentation/helpers/http.helpers';
+import { MissingParamsError, NotFound } from '@/presentation/errors';
+import {
+  badRequest, internalServerError, notFound, success,
+} from '@/presentation/helpers/http.helpers';
 import { BaseController, HttpResponse } from '@/presentation/protocols';
 
 namespace CreateOrderController {
@@ -17,19 +21,56 @@ namespace CreateOrderController {
 }
 
 export class CreateOrderController implements BaseController {
-  constructor(private readonly createOrder: CreateOrder) {}
+  constructor(
+    private readonly createOrder: CreateOrder,
+    private readonly getAccountById: GetAccountById,
+    private readonly getBusinessById: ListBusinessById,
+    private readonly emailVerificationSender: EmailVerificationSender,
+  ) {}
 
   async handle(data: CreateOrderController.Params): Promise<CreateOrderController.Result> {
     try {
       this.validate(data);
+
+      const buyerAccount = await this.getAccountById.getById({
+        accountId: data.buyerId,
+      });
+
+      const business = await this.getBusinessById.list({
+        businessId: data.businessId,
+      });
+
+      const sellerAccount = await this.getAccountById.getById({
+        accountId: business.accountId,
+      });
+
       const order = await this.createOrder.create({
         businessId: data.businessId,
         buyerId: data.buyerId,
         total: data.total,
         items: data.items,
       });
-      return success(order);
+
+      await this.emailVerificationSender.send({
+        message: `Your order has been created. OrderId: ${order.orderId}`,
+        subject: 'Order created',
+        toEmail: buyerAccount.email,
+      });
+
+      await this.emailVerificationSender.send({
+        message: `You received an order. OrderId: ${order.orderId}`,
+        subject: 'Order created',
+        toEmail: sellerAccount.email,
+      });
+
+      return success({
+        orderId: order.orderId,
+      });
     } catch (error) {
+      if (error instanceof NotFound) {
+        return notFound(error);
+      }
+
       if (error instanceof MissingParamsError) {
         return badRequest(error);
       }
--- tests/presentation/controller/order/create-order.controller.unit.test.ts
+++ tests/presentation/controller/order/create-order.controller.unit.test.ts
@@ -1,26 +1,62 @@
 import { CreateOrder } from '@/domain/usecases/order';
 import { makeCreateOrderParams } from '@/tests/domain/mocks/order.mock';
 import { CreateOrderController } from '@/presentation/controller/order/';
-import { badRequest, internalServerError, success } from '@/presentation/helpers/http.helpers';
-import { MissingParamsError } from '@/presentation/errors';
+import {
+  badRequest, internalServerError, notFound, success,
+} from '@/presentation/helpers/http.helpers';
+import { MissingParamsError, NotFound } from '@/presentation/errors';
+import { GetAccountById } from '@/domain/usecases/account';
+import { ListBusinessById } from '@/domain/usecases/business';
+import { EmailVerificationSender } from '@/data/protocols/email/email-verification-sender';
 
 describe('CreateOrderController', () => {
   let sut: CreateOrderController;
   let createOrder: CreateOrder;
+  let getAccountById: GetAccountById;
+  let listBusinessById: ListBusinessById;
+  let emailVerificationSender: EmailVerificationSender;
 
   beforeAll(() => {
     createOrder = {
       create: jest.fn(async () => Promise.resolve({ orderId: 'any_id' })),
     };
+
+    getAccountById = {
+      getById: jest.fn(async () => Promise.resolve({
+        email: 'any_email', name: 'any_name', id: 'any_id', verified: false, provider: 'credentials',
+      })),
+    };
+
+    listBusinessById = {
+      list: jest.fn(async () => Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        description: 'any_description',
+        accountId: 'any_account_id',
+        imageUrl: 'any_image_url',
+        latitude: 'any_latitude',
+        longitude: 'any_longitude',
+        street: 'any_street',
+        city: 'any_city',
+        state: 'any_state',
+        zip: 'any_zip',
+        country: 'any_country',
+      })),
+    };
+
+    emailVerificationSender = {
+      send: jest.fn(async () => Promise.resolve(true)),
+    };
+
+    sut = new CreateOrderController(createOrder, getAccountById, listBusinessById, emailVerificationSender);
   });
 
   beforeEach(() => {
-    sut = new CreateOrderController(createOrder);
+    sut = new CreateOrderController(createOrder, getAccountById, listBusinessById, emailVerificationSender);
   });
 
   it('should call CreateOrder with correct values', async () => {
     const order = makeCreateOrderParams();
-    sut = new CreateOrderController(createOrder);
 
     await sut.handle(order);
 
@@ -35,7 +71,7 @@ describe('CreateOrderController', () => {
   });
 
   it('should return internal server error if CreateOrder throws unhandled error', async () => {
-    (createOrder.create as jest.Mock).mockImplementation(async () => Promise.reject(new Error()));
+    (createOrder.create as jest.Mock).mockImplementationOnce(async () => Promise.reject(new Error()));
     const order = makeCreateOrderParams();
     const response = await sut.handle(order);
 
@@ -93,5 +129,50 @@ describe('CreateOrderController', () => {
     expect(response).toEqual(badRequest(new MissingParamsError({
       params: params.missing,
     })));
+  });
+
+  it('should call emailVerificationSender two times', async () => {
+    const order = makeCreateOrderParams();
+    await sut.handle(order);
+
+    expect(emailVerificationSender.send).toHaveBeenCalledWith({
+      message: expect.any(String),
+      subject: expect.any(String),
+      toEmail: 'any_email',
+    });
+    expect(emailVerificationSender.send).toHaveBeenCalledWith({
+      message: expect.any(String),
+      subject: expect.any(String),
+      toEmail: 'any_email',
+    });
+
+    expect(emailVerificationSender.send).toHaveBeenCalledTimes(2);
+  });
+
+  it('should call getAccountById with correct params two times', async () => {
+    const order = makeCreateOrderParams();
+    await sut.handle(order);
+
+    expect(getAccountById.getById).toHaveBeenCalledWith({
+      accountId: 'any_id',
+    });
+    expect(getAccountById.getById).toHaveBeenCalledTimes(2);
+  });
+
+  it('should call listBusinessById with correct params', async () => {
+    const order = makeCreateOrderParams();
+    await sut.handle(order);
+
+    expect(listBusinessById.list).toHaveBeenCalledWith({
+      businessId: 'any_id',
+    });
+  });
+
+  it('should return notFound if any entity was not found', async () => {
+    (getAccountById.getById as jest.Mock).mockImplementationOnce(async () => Promise.reject(new NotFound({ entity: 'Account' })));
+    const order = makeCreateOrderParams();
+    const response = await sut.handle(order);
+
+    expect(response).toEqual(notFound(new NotFound({ entity: 'Account' })));
   });
 });
