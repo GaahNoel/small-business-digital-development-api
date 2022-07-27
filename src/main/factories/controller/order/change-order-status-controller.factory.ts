30d39827061651b016e739ad4e15779d2fb232e6
GaahNoel <ga.noel@outlook.com>
2022-07-27 17:29:26 -0300

    feat: add instance of decorator of challenge to change order status route

--- src/main/factories/controller/order/change-order-status-controller.factory.ts
+++ src/main/factories/controller/order/change-order-status-controller.factory.ts
@@ -1,15 +1,21 @@
-import { DbChangeOrderStatus } from '@/data/usecases/order';
+import { DbUpdateActiveChallenge } from '@/data/usecases/challenge';
+import { DbChangeOrderStatus, DbGetOrderById } from '@/data/usecases/order';
 import { AccountPrismaRepository } from '@/infra';
+import { ChallengePrismaRepository } from '@/infra/db/prisma/challenge';
 import { OrderPrismaRepository } from '@/infra/db/prisma/order';
 import { NodeMailerAdapter } from '@/infra/email/nodemailer-adapter';
 import { env } from '@/main/config/env';
 import { ChangeOrderStatusController } from '@/presentation/controller/order';
+import { ChangeOrderStatusHandleChallengeDecorator } from '@/presentation/decorators';
 import { BaseController } from '@/presentation/protocols';
+import { BuyOrSellAnyStrategy } from '@/presentation/strategies';
 
 export const makeChangeOrderStatusControllerFactory = (): BaseController => {
   const getOrderByIdRepository = new OrderPrismaRepository();
-  const getAccountByIdRepository = new AccountPrismaRepository();
+  const accountRepository = new AccountPrismaRepository();
   const updateOrderByIdRepository = new OrderPrismaRepository();
+  const challengeRepository = new ChallengePrismaRepository();
+
   const EmailVerificationSender = new NodeMailerAdapter(
     env.emailAccount,
     env.emailAccount,
@@ -19,7 +25,12 @@ export const makeChangeOrderStatusControllerFactory = (): BaseController => {
     true,
   );
 
-  const changeOrderStatus = new DbChangeOrderStatus(getOrderByIdRepository, updateOrderByIdRepository, getAccountByIdRepository, EmailVerificationSender);
+  const changeOrderStatus = new DbChangeOrderStatus(getOrderByIdRepository, updateOrderByIdRepository, accountRepository, EmailVerificationSender);
+  const updateActiveChallenge = new DbUpdateActiveChallenge(challengeRepository);
+  const getOrderById = new DbGetOrderById(getOrderByIdRepository);
 
-  return new ChangeOrderStatusController(changeOrderStatus);
+  const changeOrderStatusController = new ChangeOrderStatusController(changeOrderStatus);
+
+  const buyOrSellAnyStrategy = new BuyOrSellAnyStrategy(updateActiveChallenge);
+  return new ChangeOrderStatusHandleChallengeDecorator(changeOrderStatusController, getOrderById, challengeRepository, accountRepository, buyOrSellAnyStrategy);
 };
